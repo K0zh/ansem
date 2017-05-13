2bd44c89a5b7bde1fb6d6ee620b005d4045b5ecb
lkh07816 <lkh07816@gmail.com>
2017-05-13 20:34:06 +0900

    [2017-05-13 이경환] 메인화면 기본 세팅

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Platform } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +7,11 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(
+    public navCtrl: NavController,
+    private platform: Platform
+  ) {
+    console.log(platform.versions());
   }
 
 }
