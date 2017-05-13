a23525287f8cb6f543c462d6f6ef15ea641cca0a
lkh07816 <lkh07816@gmail.com>
2017-05-13 18:38:30 +0900

    [2017-05-13 이경환] 각종 플러그인 기본 세팅

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { LocalNotifications } from '@ionic-native/local-notifications';
+import { Firebase } from '@ionic-native/firebase';
+import { AppVersion } from '@ionic-native/app-version';
+import { AppRate } from '@ionic-native/app-rate';
+import { AdMob } from '@ionic-native/admob';
+import { EmailComposer } from '@ionic-native/email-composer';
+import { File } from '@ionic-native/file';
+import { SQLite } from '@ionic-native/sqlite';
+
+import { CalendarModule } from "ion2-calendar";
+
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+
 
 @NgModule({
   declarations: [
@@ -20,6 +32,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     TabsPage
   ],
   imports: [
+    CalendarModule,
     BrowserModule,
     IonicModule.forRoot(MyApp)
   ],
@@ -34,7 +47,15 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    LocalNotifications,
+    Firebase,
+    AppVersion,
+    AppRate,
+    AdMob,
+    EmailComposer,
+    File,
+    SQLite
   ]
 })
 export class AppModule {}
