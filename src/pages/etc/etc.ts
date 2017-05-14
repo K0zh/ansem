a8be3026054ffb303fc842c0b5e7a983ccf9d9b6
lkh07816 <lkh07816@gmail.com>
2017-05-14 19:51:48 +0900

    [2017-05-14] 달력 샘플 추가

--- src/pages/etc/etc.ts
+++ src/pages/etc/etc.ts
@@ -1,12 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the EtcPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
+
 @Component({
   selector: 'page-etc',
   templateUrl: 'etc.html',
--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -1,23 +1,93 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the ListPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
+import {CalendarController} from "ion2-calendar/dist";
+
+
 @Component({
   selector: 'page-list',
   templateUrl: 'list.html',
 })
 export class ListPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  days:Array<any> = [];
+  
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private calendarCtrl: CalendarController
+  ) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ListPage');
   }
+  
+  ionViewWillEnter() {
+    console.log('ionViewWillEnter ListPage');
+  }
+  
+  ionViewWillUnload() {
+    console.log('ionViewWillUnload ListPage');
+  }
 
+  
+  daysConfig() {
+    let _daysConfig = [
+      {
+        date:new Date(2017,1,1),
+        subTitle:'New Year\'s',
+        marked:true,
+        cssClass:'day-danger',
+      },
+      {
+        date:new Date(2017,1,14),
+        subTitle:'Valentine\'s',
+      },
+      {
+        date:new Date(2017,3,1),
+        subTitle:'April Fools',
+        marked:true
+      },
+      {
+        date:new Date(2017,3,7),
+        subTitle:'World Health',
+      },
+      {
+        date:new Date(2017,4,31),
+        subTitle:'No-Smoking',
+      },
+      {
+        date:new Date(2017,5,1),
+        subTitle:'Children\'s',
+      }
+    ];
+
+//    for(let i = 0;  i < 31; i++){
+//      this.days.push({
+//        date:new Date(2017,0,i+1),
+//        subTitle:`$${i+1}`
+//      })
+//    }
+
+    //_daysConfig.push(...this.days);
+
+    this.calendarCtrl.openCalendar({
+      title: '전체보기',
+      monthTitle: 'yyyy년 MM월',
+      weekdaysTitle: ["일", "월", "화", "수", "목", "금", "토"],
+      closeLabel: '취소',
+      from: new Date(2017,0,1),
+      to: new Date(),
+      defaultDate: new Date(),
+      cssClass: 'my-class',
+      daysConfig: _daysConfig
+    }).then((res:any) => {
+      //alert(1);
+      console.log(res);
+    }).catch((res:any) => {
+      console.log(res);
+    });
+  }
+  
 }
