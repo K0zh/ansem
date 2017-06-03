2559d0e86910a9be5ec7dbf891280b5893a2d61d
lkh07816 <lkh07816@gmail.com>
2017-06-03 19:57:09 +0900

    [2017-06-03 이경환] config.xml / 알림설정 수정

--- src/pages/etc/notification/notification.ts
+++ src/pages/etc/notification/notification.ts
@@ -1,21 +1,23 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams, ViewController } from 'ionic-angular';
 
+import * as moment from 'moment';
+
 @Component({
   selector: 'page-notification',
   templateUrl: 'notification.html',
 })
 export class NotificationPage {
   
-  is_notification:boolean;
-  notification_time:any = "15:00";
+  is_notification: boolean;
+  notification_time: any;
   
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
     public viewCtrl: ViewController
   ) {
-      
+    this.notification_time = moment().format("HH:mm");
   }
 
   ionViewDidLoad() {
