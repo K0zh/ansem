e9719d61879866367d8476845553d0fc272a19f1
lkh07816 <lkh07816@gmail.com>
2017-07-02 16:59:28 +0900

    [2017-07-02 이경환] 네트워크 체크

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Platform, AlertController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { Network } from '@ionic-native/network';
 
 import { TabsPage } from '../pages/tabs/tabs';
 
@@ -14,13 +15,30 @@ export class MyApp {
   constructor(
     private platform: Platform,
     private statusBar: StatusBar,
-    private splashScreen: SplashScreen
+    private splashScreen: SplashScreen,
+    private network: Network,
+    private alertCtrl: AlertController
   ) {
     this.initializeApp();
   }
   
   initializeApp() {
     this.platform.ready().then(() => {  //this.statusBar.backgroundColorByHexString('#171717');
+      const disconnectSubscription = this.network.onDisconnect().subscribe(() => {
+        let alert = this.alertCtrl.create({
+          title: '알림',
+          subTitle: "네트워크 상태를 확인해주세요",
+          buttons: [{
+            text: "확인",
+            handler: () => {
+              this.platform.exitApp();
+            }
+          }]
+        });
+        alert.present();
+        disconnectSubscription.unsubscribe();
+      });
+
       this.statusBar.styleDefault();
       setTimeout(() => {
         this.splashScreen.hide();
