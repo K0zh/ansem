38518b389e9d666197c25b7a68b623d08f1e8270
0zh <bing9870@gmail.com>
2017-07-16 15:49:36 +0900

    달력 삭제

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -1,8 +1,5 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams, ModalController } from 'ionic-angular';
-
-import { CalendarController } from "ion2-calendar/dist";
-
 import { PostsPage } from '../posts/posts';
 
 import { Platform } from 'ionic-angular';
@@ -29,7 +26,6 @@ export class ListPage {
     public modalCtrl: ModalController,
     public platform: Platform,
     public sqlite: SQLiteProvider,
-    private calendarCtrl: CalendarController,
     public localStorage: LocalStorageProvider
   ) {
     localStorage.selectTodayBgImg().then((data) => {
@@ -94,64 +90,4 @@ export class ListPage {
       console.log(e);
     });
   }
-
-  
-  daysConfig() {
-    let _daysConfig = [
-      {
-        date:new Date(2017,1,1),
-        subTitle:'New Year\'s',
-        marked:true,
-        cssClass:'day-danger',
-      },
-      {
-        date:new Date(2017,1,14),
-        subTitle:'Valentine\'s',
-      },
-      {
-        date:new Date(2017,3,1),
-        subTitle:'April Fools',
-        marked:true
-      },
-      {
-        date:new Date(2017,3,7),
-        subTitle:'World Health',
-      },
-      {
-        date:new Date(2017,4,31),
-        subTitle:'No-Smoking',
-      },
-      {
-        date:new Date(2017,5,1),
-        subTitle:'Children\'s',
-      }
-    ];
-
-//    for(let i = 0;  i < 31; i++){
-//      this.days.push({
-//        date:new Date(2017,0,i+1),
-//        subTitle:`$${i+1}`
-//      })
-//    }
-
-    //_daysConfig.push(...this.days);
-
-    this.calendarCtrl.openCalendar({
-      title: '전체보기',
-      monthTitle: 'yyyy년 MM월',
-      weekdaysTitle: ["일", "월", "화", "수", "목", "금", "토"],
-      closeLabel: '취소',
-      from: new Date(2017,0,1),
-      to: new Date(),
-      defaultDate: new Date(),
-      cssClass: 'my-class',
-      daysConfig: _daysConfig
-    }).then((res:any) => {
-      //alert(1);
-      console.log(res);
-    }).catch((res:any) => {
-      console.log(res);
-    });
-  }
-
 }
